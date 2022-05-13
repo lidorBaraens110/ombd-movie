e64144eb1893f0f79ae31d531a3db9e51173795b
lidorBaraens110 <51084061+lidorBaraens110@users.noreply.github.com>
2022-05-13 14:32:27 +0300

    config redis with new host

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,12 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import * as redis from 'redis';
 
-dotenv.config()
-export const redisClient = redis.createClient();
+dotenv.config();
+
+export const redisClient =  redis.createClient({
+    url: 'redis://redis-17516.c9.us-east-1-2.ec2.cloud.redislabs.com:17516/',
+    password: 'L5VbcxzFzsReXoRSP2X2UBRvr3TcTVYD'
+});
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 redisClient.connect();
